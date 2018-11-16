00b2fdf023d0cb382f6cb43b2c0d470f8cb8f4e8
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-11-16 16:28:19 -0500

    updated the login and signup pages

--- models/schema.sql
+++ models/schema.sql
@@ -1,4 +1,4 @@
---drops database if exists (fine while in development)--
+-- drops database if exists (fine while in development) --
 DROP DATABASE IF EXISTS project2db;
---creates our lousy database with an insanely original name--
+-- creates our lousy database with an insanely original name --
 CREATE DATABASE project2db;
