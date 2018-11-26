6a3c1bd1b02255981b9bb9515f7948241b5abf6b
H. B. Young <HarryBeckerYoung@gmail.com>
2018-11-26 16:39:18 -0500

    console logging some junk

--- models/index.js
+++ models/index.js
@@ -8,6 +8,8 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
+console.log(config.use_env_variable)
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
